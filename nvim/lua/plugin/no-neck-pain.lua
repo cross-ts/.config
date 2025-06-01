8edb28a9c7f0f1ff6cb79a746f27470144943a9e
cross-ts <ts.kouhei@gmail.com>
2025-06-02 00:59:40 +0900

    disable no-neck-pain

--- nvim/lua/plugin/no-neck-pain.lua
+++ nvim/lua/plugin/no-neck-pain.lua
@@ -1,6 +1,7 @@
 -- ☕ Dead simple yet super extensible plugin to center the currently focused buffer to the middle of the screen.
 return {
   'shortcuts/no-neck-pain.nvim',
+  enabled = false,
   cmd = {
     'NoNeckPain',
   },
