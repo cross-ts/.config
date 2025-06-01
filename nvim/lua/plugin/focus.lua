392f4fd39a478ea9ef338a5907539c6841e29586
cross-ts <ts.kouhei@gmail.com>
2025-06-02 00:59:49 +0900

    Add focus

---
+++ nvim/lua/plugin/focus.lua
@@ -0,0 +1,9 @@
+-- Auto-Focusing and Auto-Resizing Splits/Windows for Neovim written in Lua. A full suite of window management enhancements. Vim splits on steroids!
+return {
+  'nvim-focus/focus.nvim',
+  event = {
+    'BufNewFile',
+    'BufReadPre',
+  },
+  config = true,
+}
