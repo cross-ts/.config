e3ad74c32dd33e8bf2289e19394e5bd84a29cf02
cross-ts <ts.kouhei@gmail.com>
2025-12-10 00:12:34 +0900

    checkhealthを通すようにmiseを設定

--- nvim/lua/init/variable.lua
+++ nvim/lua/init/variable.lua
@@ -2,4 +2,8 @@
 vim.g.mapleader = ' '
 
 -- python path
-vim.g.python3_host_prog = os.getenv("XDG_DATA_HOME") .. "/mise/shims/python"
+vim.g.python3_host_prog = vim.fn.exepath('python3')
+
+-- disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
