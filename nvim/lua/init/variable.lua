13ecd4f4cbaee8407636674b0603f2fa92dd4201
cross-ts <ts.kouhei@gmail.com>
2025-12-10 00:39:50 +0900

    vim周りの諸々を解決

--- nvim/lua/init/variable.lua
+++ nvim/lua/init/variable.lua
@@ -1,9 +1,10 @@
 -- Leader Key
 vim.g.mapleader = ' '
 
--- python path
+-- Language Providers
 vim.g.python3_host_prog = vim.fn.exepath('python3')
+vim.g.ruby_host_prog = vim.fn.exepath('ruby')
 
--- disable providers
+-- Disabled Providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
