0847a314fbf0531b53491d95f748eaea8c3703d0
cross-ts <ts.kouhei@gmail.com>
2026-01-02 16:40:32 +0900

    Fix deprecated

--- nvim/lua/plugin/nvim-cmp.lua
+++ nvim/lua/plugin/nvim-cmp.lua
@@ -11,7 +11,6 @@ return {
   },
   event = {
     'InsertEnter',
-    'LspAttach',
     'CmdLineEnter',
   },
   config = function()
@@ -19,7 +18,7 @@ return {
     local lspkind = require('lspkind')
 
     local has_words_before = function()
-      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+      if vim.api.nvim_get_option_value('buftype', {}) == 'prompt' then
         return false
       end
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
