eeab00c090e887a1bf2088d0d9ba717cdb96e8aa
cross-ts <ts.kouhei@gmail.com>
2026-04-02 13:04:07 +0900

    nvim-treesitterの設定ミス

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
   },
   lazy = false,
   config = function()
-    require('nvim-treesitter.config').setup({})
+    require('nvim-treesitter').setup({})
     vim.api.nvim_create_autocmd('FileType', {
       group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
       callback = function()
