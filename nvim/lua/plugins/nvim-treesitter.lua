dc095718fdf42eb3d3acf3679461e9f0c03aa15b
cross-ts <ts.kouhei@gmail.com>
2026-04-12 15:33:32 +0900

    nvim-treesitterの設定を微修正

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -8,13 +8,30 @@ return {
     'RRethy/nvim-treesitter-endwise',
   },
   lazy = false,
-  config = function()
-    require('nvim-treesitter').setup({})
+  init = function()
+    local ensureInstalled = {
+      -- Languages I use often
+      'lua', 'python', 'typescript', 'php', 'phpdoc', 'ruby',
+      -- Config files
+      'json', 'yaml', 'git_config', 'gitignore',
+      'hcl', 'terraform', 'nginx',
+      -- Misc
+      'gitcommit',
+    }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+
     vim.api.nvim_create_autocmd('FileType', {
       group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
       callback = function()
         pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
-  end
+  end,
 }
