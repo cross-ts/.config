61a54bf907884b440dfd3061b7f8bbb107b15b1e
cross-ts <ts.kouhei@gmail.com>
2026-05-03 04:03:22 +0900

    yamlのschemaを追加

--- nvim/lua/configs/lsp/yamlls.lua
+++ nvim/lua/configs/lsp/yamlls.lua
@@ -7,5 +7,26 @@ return {
     "yaml.gitlab",
     "yaml.helm-values",
     "yaml.neon",
+  },
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/dependabot-2.0.json"] = ".github/dependabot.yml",
+        ["https://json.schemastore.org/github-workflow.json"] = {
+          ".github/workflows/*.yml",
+          ".github/workflows/*.yaml",
+        },
+        ["https://json.schemastore.org/github-action.json"] = {
+          "action.yml",
+          "action.yaml",
+        },
+        ["https://raw.githubusercontent.com/compose-spec/compose-go/refs/heads/main/schema/compose-spec.json"] = {
+          "compose.yml",
+          "compose.yaml",
+          "docker-compose.yml",
+          "docker-compose.yaml",
+        },
+      }
+    }
   }
 }
