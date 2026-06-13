40fd4080bfd44567d4a8a23ece74b6d18093bc46
cross-ts <ts.kouhei@gmail.com>
2026-06-13 12:51:28 +0900

    disable all vim provider

--- nvim/lua/configs/init/settings.lua
+++ nvim/lua/configs/init/settings.lua
@@ -2,12 +2,16 @@
 vim.g.mapleader = ' '
 
 -- Language Providers
-vim.g.python3_host_prog = vim.fn.exepath('python3')
-vim.g.ruby_host_prog = vim.fn.exepath('ruby')
+-- vim.g.node_host_prog = vim.fn.exepath('node')
+-- vim.g.perl_host_prog = vim.fn.exepath('perl')
+-- vim.g.python3_host_prog = vim.fn.exepath('python3')
+-- vim.g.ruby_host_prog = vim.fn.exepath('ruby')
 
 -- Disabled Providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 -- Colors
 vim.opt.termguicolors = true
